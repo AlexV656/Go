c878ff05eb7705dd2cfa3d41dfe6b2e23a6839ec
AlexV656 <vasylievalexandr2@gmail.com>
2026-03-08 01:26:06 +0100

    trying_export_import

--- prog_1.go
+++ prog_1.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/AlexV656/Go/prog_2"
+)
+
+var ExportedVariable string = "Hello, World!"
 
 func main() {
 	var msg string = "Hello"
+	fmt.Println(ExportedVariable)
+	fmt.Println(prog_2.ExportedVariable)
 	for i := 1; i < 10; i++ {
 		fmt.Println(msg)
 	}
